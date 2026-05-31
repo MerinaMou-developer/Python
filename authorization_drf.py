a08612b7ff63ea107298b8d894fd820de8af73a6
Merina Mou <merinamou7@gmail.com>
2026-05-31 15:08:54 +0600

    has object permission

--- authorization_drf.py
+++ authorization_drf.py
@@ -1,7 +1,5 @@
 from rest_framework.permissions import BasePermission
 
 class IsOwnerOrAdmin(BasePermission):
-    def has_objkect_permission(self,request,view,obj):
-        if request.user.is_admin:
-            return True
-        return obj.user=request.user
+    def has_object_permission(self, request, view, obj):
+        return (obj.user == request.user or request.user.is_staff)
